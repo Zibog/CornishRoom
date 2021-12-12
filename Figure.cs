a9cb7a4a027b2b828137a2ec32c7befa9f74a377
Zibog <dmsidak@gmail.com>
2021-12-12 22:06:20 +0300

    Add Cube and Sphere classes

--- Figure.cs
+++ Figure.cs
@@ -19,4 +19,34 @@ namespace CornishRoom
             Color = color;
         }
     }
+
+    public class Cube : Figure
+    {
+        public Point3D From { get; }
+        public Point3D To { get; }
+        public Point3D Normal { get; }
+
+        public Cube(FigureType type, Material material, Color color,
+            Point3D from, Point3D to, Point3D normal)
+            : base(type, material, color)
+        {
+            From = from;
+            To = to;
+            Normal = normal;
+        }
+    }
+
+    public class Sphere : Figure
+    {
+        public Point3D Center { get; }
+        public double Radius { get; }
+        
+        public Sphere(Material material, Color color,
+            Point3D center, double radius)
+            : base(FigureType.Sphere, material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+    }
 }
