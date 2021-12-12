fecdb340f68b03fa17cc781fff4a58b915e312c5
Zibog <dmsidak@gmail.com>
2021-12-12 22:21:37 +0300

    Divide Figure class to specific classes

---
+++ Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace CornishRoom
+{
+    public class Cube : Figure
+    {
+        public Point3D From { get; }
+        public Point3D To { get; }
+        public Point3D Normal { get; }
+
+        public Cube(FigureType type, Material material, Color color,
+            Point3D from, Point3D to, Point3D normal)
+            : base(type, material, color)
+        {
+            From = from;
+            To = to;
+            Normal = normal;
+        }
+    }
+}
---
+++ Sphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace CornishRoom
+{
+    public class Sphere : Figure
+    {
+        public Point3D Center { get; }
+        public double Radius { get; }
+        
+        public Sphere(Material material, Color color,
+            Point3D center, double radius)
+            : base(FigureType.Sphere, material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+    }
+}
