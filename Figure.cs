485da6f9c6bea0701f82a6f9b5db1868cd587a02
Zibog <dmsidak@gmail.com>
2021-12-12 21:52:49 +0300

    Add abstract Figure class

---
+++ Figure.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace CornishRoom
+{
+    public enum FigureType { Cube = 0, Sphere, Wall }
+    
+    public enum Material { Matte = 0, Transparent, Reflective }
+    
+    public abstract class Figure
+    {
+        public FigureType FigureType { get; }
+        public Material Material { get; set; }
+        public Color Color { get; set; }
+
+        public Figure(FigureType type, Material material, Color color)
+        {
+            FigureType = type;
+            Material = material;
+            Color = color;
+        }
+    }
+}
