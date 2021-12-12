e1b2d9c8e55c40118faca7468c5703ea847882a2
Zibog <dmsidak@gmail.com>
2021-12-12 22:11:55 +0300

    Add Light class

---
+++ Light.cs
@@ -0,0 +1,14 @@
+﻿namespace CornishRoom
+{
+    public class Light
+    {
+        public Point3D Position { get; }
+        public double Intensity { get; }
+
+        public Light(Point3D position, double intensity)
+        {
+            Position = position;
+            Intensity = intensity;
+        }
+    }
+}
