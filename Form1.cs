50a5f3dd7023a47d929037609150c29b7d1be737
Zibog <dmsidak@gmail.com>
2021-12-17 00:39:52 +0300

    Change colors

--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,12 @@ namespace CornishRoom
 
             _figures = new List<Figure>();
             InitRoom(25, 25, 50);
-            InitCube(Material.Transparent, Color.DarkKhaki, new Point3D(-15, -21, 20), 4);
-            InitCube(Material.Reflective, Color.GreenYellow, new Point3D(10, -22, 0), 3);
             InitCube(Material.Matte, Color.Red, new Point3D(0, 0, 0), 5);
+            InitCube(Material.Reflective, Color.Orange, new Point3D(10, -22, 0), 3);
+            InitCube(Material.Transparent, Color.Blue, new Point3D(-15, -21, 20), 4);
+            InitSphere(Material.Matte, Color.Indigo, new Point3D(-15, -10, -25), 5);
             InitSphere(Material.Reflective, Color.Cornsilk, new Point3D(10, -15, 15), 10);
             InitSphere(Material.Transparent, Color.Chartreuse, new Point3D(10, -15, 0), 3);
-            InitSphere(Material.Matte, Color.Indigo, new Point3D(-15, -10, -25), 5);
 
             var lights = new List<Light>
             {
@@ -36,12 +36,12 @@ namespace CornishRoom
 
         private void InitRoom(double x, double y, double z)
         {
-            InitWall(Material.Matte, Color.Violet, new Point3D(-x, -y, z), new Point3D(x, y, z), new Point3D(0, 0, -1));
+            InitWall(Material.Matte, Color.Fuchsia, new Point3D(-x, -y, z), new Point3D(x, y, z), new Point3D(0, 0, -1));
             InitWall(Material.Reflective, Color.White, new Point3D(-x, -y, -z), new Point3D(x, -y, z), new Point3D(0, 1, 0));
             InitWall(Material.Matte, Color.DarkMagenta, new Point3D(-x, y, -z), new Point3D(x, y, z), new Point3D(0, -1, 0));
             InitWall(Material.Matte, Color.LightBlue, new Point3D(-x, -y, -z), new Point3D(-x, y, z), new Point3D(1, 0, 0));
-            InitWall(Material.Reflective, Color.IndianRed, new Point3D(x, -y, -z), new Point3D(x, y, z), new Point3D(-1, 0, 0));
-            InitWall(Material.Matte, Color.LightPink, new Point3D(-x, -y, -z), new Point3D(x, y, -z), new Point3D(0, 0, 1));
+            InitWall(Material.Reflective, Color.Gold, new Point3D(x, -y, -z), new Point3D(x, y, z), new Point3D(-1, 0, 0));
+            InitWall(Material.Matte, Color.DarkBlue, new Point3D(-x, -y, -z), new Point3D(x, y, -z), new Point3D(0, 0, 1));
         }
         
         private void InitWall(Material material, Color color, Point3D from, Point3D to, Point3D normal)
@@ -54,8 +54,8 @@ namespace CornishRoom
             var p1 = center - distanceToPlane;
             var p2 = center + distanceToPlane;
 
-            InitPlane(material, color, new Point3D(p1.X, p1.Y, p2.Z), new Point3D(p2.X, p2.Y, p2.Z), new Point3D(0, 0, 1));
             InitPlane(material, color, new Point3D(p1.X, p1.Y, p1.Z), new Point3D(p2.X, p2.Y, p1.Z), new Point3D(0, 0, -1));
+            InitPlane(material, color, new Point3D(p1.X, p1.Y, p2.Z), new Point3D(p2.X, p2.Y, p2.Z), new Point3D(0, 0, 1));
             
             InitPlane(material, color, new Point3D(p1.X, p1.Y, p1.Z), new Point3D(p1.X, p2.Y, p2.Z), new Point3D(-1, 0, 0));
             InitPlane(material, color, new Point3D(p2.X, p1.Y, p1.Z), new Point3D(p2.X, p2.Y, p2.Z), new Point3D(1, 0, 0));
